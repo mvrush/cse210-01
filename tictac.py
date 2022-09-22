89855f7af488f646565c81b31fa0cd89ba749b53
mvrush <mvrush@gmail.com>
2022-09-21 23:45:22 -0600

    Comment In Code

---
+++ tictac.py
@@ -0,0 +1,9 @@
+"""
+File: tictac.py
+Author: Matt Rushton
+
+Purpose: Two players play a game of Tic-Tac-Toe.
+Input: Users chooses a spot on a grid to place either and X or an O.
+Output: Show users placement on grid and inform when a victory is attained.
+
+"""
